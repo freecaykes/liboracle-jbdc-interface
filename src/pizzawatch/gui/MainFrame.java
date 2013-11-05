c2f474d5c0917860afa6e786945483b9ea454af8
Riad Zitouni <riyyad@yahoo.fr>
2013-11-05 12:15:52 -0800

    Added the PendingOrdersFrame GUI to store pending orders Updated the MainFrame class to open the PendingOrdersFrame when the view pending orders button is pressed

--- src/pizzawatch/gui/MainFrame.java
+++ src/pizzawatch/gui/MainFrame.java
@@ -204,6 +204,9 @@ public class MainFrame extends javax.swing.JFrame
     private void btViewPendingOrdersActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btViewPendingOrdersActionPerformed
     {//GEN-HEADEREND:event_btViewPendingOrdersActionPerformed
         //TODO
+    	
+    	PendingOrdersFrame frame = new PendingOrdersFrame();
+    	frame.setVisible(true);
     }//GEN-LAST:event_btViewPendingOrdersActionPerformed
 
     private void btRequestCancelActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btRequestCancelActionPerformed
