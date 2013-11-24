76969f3b05be16f58f323707960692e433809442
John Russ Agawin <jragawin_12@hotmail.com>
2013-11-23 22:32:49 -0800

    variable names changes and some implementation

--- src/pizzawatch/gui/AddOrEditUserFrame.java
+++ src/pizzawatch/gui/AddOrEditUserFrame.java
@@ -6,12 +6,14 @@
 
 package pizzawatch.gui;
 
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author John
  */
 public class AddOrEditUserFrame extends javax.swing.JFrame {
-
     /**
      * Creates new form NewUserFrame
      */
@@ -19,6 +21,28 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
         initComponents();
     }
     
+    private void handleSubmitAttempt(){
+        String newUserID = userID.getText();
+        String newPassword = new String (password.getPassword());
+        String newPasswordReType = new String (passwordReType.getPassword());
+        String newFirstName = firstName.getText();
+        String newLastName = lastName.getText();
+        //String newCardNumber = new JPasswordField();
+        
+        //Checks if the two passwords match
+        if (!newPassword.equals(newPasswordReType)){
+            JOptionPane.showMessageDialog (this, "Passwords do not match. Please try again.");
+            return;
+        }
+        
+        //Checks if all fields are filled
+        if (newUserID.isEmpty() || newPassword.isEmpty() || newPasswordReType.isEmpty()
+                || newFirstName.isEmpty() || newLastName.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Some fields are blank. All fields are required.");
+            return;
+        }
+    }
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -28,36 +52,35 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        firstNameLabel = new javax.swing.JLabel();
+        lastNameLabel = new javax.swing.JLabel();
         lbTitle = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        cardNumberLabel = new javax.swing.JLabel();
+        userIDLabel = new javax.swing.JLabel();
         submitButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        passwordLabel = new javax.swing.JLabel();
+        reTypePasswordLabel = new javax.swing.JLabel();
         password = new javax.swing.JPasswordField();
         passwordReType = new javax.swing.JPasswordField();
         cardNumber = new javax.swing.JFormattedTextField();
-        jLabel9 = new javax.swing.JLabel();
+        createNewAccountLabel = new javax.swing.JLabel();
         firstName = new javax.swing.JTextField();
         lastName = new javax.swing.JTextField();
         userID = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel2.setText("First Name");
+        firstNameLabel.setText("First Name");
 
-        jLabel3.setText("Last Name");
+        lastNameLabel.setText("Last Name");
 
         lbTitle.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         lbTitle.setText("TMNT Pizza Watch");
 
-        jLabel4.setText("Card Number");
+        cardNumberLabel.setText("Card Number");
 
-        jLabel6.setText("User ID");
+        userIDLabel.setText("User ID");
 
         submitButton.setText("Submit");
         submitButton.addActionListener(new java.awt.event.ActionListener() {
@@ -68,21 +91,18 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
 
         cancelButton.setText("Cancel");
 
-        jLabel7.setText("Password");
+        passwordLabel.setText("Password");
 
-        jLabel8.setText("Re-type Password");
+        reTypePasswordLabel.setText("Re-type Password");
 
-        password.setText("jPasswordField1");
-
-        passwordReType.setText("jPasswordField2");
         passwordReType.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 passwordReTypeActionPerformed(evt);
             }
         });
 
-        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel9.setText("CREATE A NEW ACCOUNT");
+        createNewAccountLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        createNewAccountLabel.setText("CREATE A NEW ACCOUNT");
 
         userID.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -109,9 +129,8 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lbTitle)
-                            .addComponent(jLabel8)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel7))
+                            .addComponent(reTypePasswordLabel)
+                            .addComponent(passwordLabel))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -121,12 +140,12 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
                                 .addComponent(lastName))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel4)
-                                    .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(cardNumberLabel)
+                                    .addComponent(createNewAccountLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel2)
+                                        .addComponent(firstNameLabel)
                                         .addGap(274, 274, 274)
-                                        .addComponent(jLabel3)))
+                                        .addComponent(lastNameLabel)))
                                 .addGap(0, 0, Short.MAX_VALUE)))
                         .addGap(10, 10, 10))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -135,7 +154,7 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
                             .addComponent(passwordReType, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(password, javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addComponent(jLabel6)
+                                .addComponent(userIDLabel)
                                 .addGap(0, 0, Short.MAX_VALUE)))
                         .addContainerGap())))
         );
@@ -145,48 +164,47 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(lbTitle)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(createNewAccountLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(13, 13, 13)
-                .addComponent(jLabel6)
+                .addComponent(userIDLabel)
                 .addGap(5, 5, 5)
                 .addComponent(userID, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel7)
+                .addComponent(passwordLabel)
                 .addGap(5, 5, 5)
                 .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel8)
+                .addComponent(reTypePasswordLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(passwordReType, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
+                    .addComponent(firstNameLabel)
+                    .addComponent(lastNameLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lastName, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
                     .addComponent(firstName))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel4)
+                .addComponent(cardNumberLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(cardNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(submitButton)
-                            .addComponent(cancelButton))
-                        .addGap(24, 24, 24))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(submitButton)
+                    .addComponent(cancelButton))
+                .addGap(24, 24, 24))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void submitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitButtonActionPerformed
-        // TODO add your handling code here:
+        handleSubmitAttempt();
     }//GEN-LAST:event_submitButtonActionPerformed
-
+    private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt){
+        
+    }
     private void passwordReTypeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordReTypeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordReTypeActionPerformed
@@ -233,20 +251,19 @@ public class AddOrEditUserFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cancelButton;
     private javax.swing.JFormattedTextField cardNumber;
+    private javax.swing.JLabel cardNumberLabel;
+    private javax.swing.JLabel createNewAccountLabel;
     private javax.swing.JTextField firstName;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel firstNameLabel;
     private javax.swing.JTextField lastName;
+    private javax.swing.JLabel lastNameLabel;
     private javax.swing.JLabel lbTitle;
     private javax.swing.JPasswordField password;
+    private javax.swing.JLabel passwordLabel;
     private javax.swing.JPasswordField passwordReType;
+    private javax.swing.JLabel reTypePasswordLabel;
     private javax.swing.JButton submitButton;
     private javax.swing.JTextField userID;
+    private javax.swing.JLabel userIDLabel;
     // End of variables declaration//GEN-END:variables
 }
